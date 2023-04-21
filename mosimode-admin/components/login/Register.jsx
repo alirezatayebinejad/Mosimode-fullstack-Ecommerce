8c8f866c61696a413b10dd10bc380e94953f2401
vibracode <codevibra@gmail.com>
2023-04-22 01:33:03 +0430

    fixing login page inputs

--- mosimode-admin/components/login/Register.jsx
+++ mosimode-admin/components/login/Register.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import styles from "@/components/login/Register.module.css";
+import { useState } from "react";
 
 function Register() {
+	const [formState, setFormState] = useState({ name: "", email: "", password: "" });
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormState((prevState) => ({ ...prevState, [name]: value }));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState); // Do something with the form data
+	};
 	return (
 		<div className={styles.card}>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<h2 className={styles.title}>Register</h2>
 				<label htmlFor="name">Name:</label>
-				<input type="text" id="name" name="name" required />
+				<input type="text" id="name" name="name" value={formState.name} onChange={handleChange} required />
 
 				<label htmlFor="email">Email:</label>
-				<input type="email" id="email" name="email" required />
+				<input type="email" id="email" name="email" value={formState.email} onChange={handleChange} required />
 
 				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" name="password" required />
+				<input type="password" id="password" name="password" value={formState.password} onChange={handleChange} required />
 
 				<button type="submit">register</button>
 			</form>
--- mosimode-admin/components/login/Signincard.jsx
+++ mosimode-admin/components/login/Signincard.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import styles from "@/components/login/Signincard.module.css";
+import { useState } from "react";
 
 function Signincard() {
+	const [formState, setFormState] = useState({ email: "", password: "" });
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setFormState((prevState) => ({ ...prevState, [name]: value }));
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(formState); // Do something with the form data
+	};
 	return (
 		<div className={styles.card}>
 			<h2 className={styles.title}>Sign In</h2>
-			<form className={styles.form}>
+			<form className={styles.form} onSubmit={handleSubmit}>
 				<label htmlFor="email">Email:</label>
-				<input type="email" id="email" name="email" required />
+				<input type="email" id="email" name="email" value={formState.name} onChange={handleChange} required />
 
 				<label htmlFor="password">Password:</label>
-				<input type="password" id="password" name="password" required />
+				<input type="password" id="password" name="password" value={formState.pass} onChange={handleChange} required />
 
 				<button type="submit">Sign In</button>
 			</form>
