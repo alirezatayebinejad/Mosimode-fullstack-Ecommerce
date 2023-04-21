95928790fa0e63149e5be2e61ef7865abbe8927d
vibracode <codevibra@gmail.com>
2023-04-22 02:12:00 +0430

    created the database config and user model

--- mosimode-admin/next.config.js
+++ mosimode-admin/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    DB_URI: "mongodb://localhost:27017/next13-auth",
+    NEXTAUTH_SECRET: "9Gt8#sJn7qL3@vDp5&fE",
+  }
 }
 
 module.exports = nextConfig;
