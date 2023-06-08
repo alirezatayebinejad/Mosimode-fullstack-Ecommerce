69169f057244567a328832663048dbead581ceef
vibracode <codevibra@gmail.com>
2023-06-08 19:17:21 +0430

    auth options in header added for logged in or not

--- mosimode-shop/src/components/Header/BurgerMenu.jsx
+++ mosimode-shop/src/components/Header/BurgerMenu.jsx
@@ -4,9 +4,14 @@ import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const BurgerMenu = () => {
+	const { data, status } = useSession();
 	const [burgermenu, setBurgermenu] = useState(false);
+	const router = useRouter();
+
 	return (
 		<div className={styles.burgermenu}>
 			<MenuIcon onClick={() => setBurgermenu(true)} />
@@ -20,18 +25,23 @@ const BurgerMenu = () => {
 					</div>
 					<nav className={styles.list_nav}>
 						<ul>
-							<li>
+							<li className={router.pathname === "/" ? styles.active : ""}>
 								<Link href="/">Home</Link>
 							</li>
-							<li>
+							<li className={router.pathname === "/shop" ? styles.active : ""}>
 								<Link href="/shop">Shop</Link>
 							</li>
-							<li>
+							<li className={router.pathname === "/about" ? styles.active : ""}>
 								<Link href="/about">About</Link>
 							</li>
-							<li>
+							<li className={router.pathname === "/contact" ? styles.active : ""}>
 								<Link href="/contact">Contact</Link>
 							</li>
+							{status === "authenticated" && data !== null && (
+								<li className={router.pathname === "/dashboard" ? styles.active : ""}>
+									<Link href="/dashboard">Dashboard</Link>
+								</li>
+							)}
 						</ul>
 					</nav>
 				</div>
--- mosimode-shop/src/components/Header/Header.jsx
+++ mosimode-shop/src/components/Header/Header.jsx
@@ -7,11 +7,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { setIsCartOpen } from "../../store/cartSlice";
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
 import BurgerMenu from "./BurgerMenu";
 import ShoppingCart from "./ShoppingCart";
 import SearchPopup from "../Popups/SearchPopup";
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+	const { data, status } = useSession();
 	const dispatch = useDispatch();
 	const router = useRouter();
 	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -28,61 +31,82 @@ const Header = () => {
 						<h3>Mosimode</h3>
 					</div>
 				</Link>
-				<div className={styles.list}>
-					<nav className={styles.list_nav}>
-						<ul>
-							<li>
-								<Link className={router.pathname === "/" ? styles.active : ""} href="/">
-									Home
-								</Link>
-							</li>
-							<li className={styles.shop_list} onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}>
-								<Link className={router.pathname === "/shop" ? styles.active : ""} href="/shop">
-									Shop
-								</Link>
-								<div className={styles.dropdown_content}>
-									{isDropdownOpen && (
-										<ul>
-											<li>
-												<Link href="/shop?category=men's clothing">Men's Clothing</Link>
-											</li>
-											<li>
-												<Link href="/shop?category=women's clothing">Women's Clothing</Link>
-											</li>
-											<li>
-												<Link href="/shop?category=electronics">Electronics</Link>
-											</li>
-											<li>
-												<Link href="/shop?category=jewelery">Jewelery</Link>
-											</li>
-										</ul>
-									)}
-								</div>
-							</li>
-							<li>
-								<Link className={router.pathname === "/about" ? styles.active : ""} href="/about">
-									About
-								</Link>
-							</li>
-							<li>
-								<Link className={router.pathname === "/contact" ? styles.active : ""} href="/contact">
-									Contact
-								</Link>
-							</li>
-						</ul>
-					</nav>
-					<div className={styles.list_icon}>
-						<li onClick={() => setIsSearchOpen(true)}>
-							<SearchIcon />
+				<nav className={styles.list_nav}>
+					<ul>
+						<li>
+							<Link className={router.pathname === "/" ? styles.active : ""} href="/">
+								Home
+							</Link>
 						</li>
-						<li className={styles.shoppingIcon_li} onClick={() => dispatch(setIsCartOpen({}))}>
-							{cartCount > 0 && <span>{cartCount}</span>}
-							<ShoppingBasketIcon />
+						<li className={styles.shop_list} onMouseEnter={() => setIsDropdownOpen(true)} onMouseLeave={() => setIsDropdownOpen(false)}>
+							<Link className={router.pathname === "/shop" ? styles.active : ""} href="/shop">
+								Shop
+							</Link>
+							<div className={styles.dropdown_content}>
+								{isDropdownOpen && (
+									<ul>
+										<li>
+											<Link href="/shop?category=men's clothing">Men's Clothing</Link>
+										</li>
+										<li>
+											<Link href="/shop?category=women's clothing">Women's Clothing</Link>
+										</li>
+										<li>
+											<Link href="/shop?category=electronics">Electronics</Link>
+										</li>
+										<li>
+											<Link href="/shop?category=jewelery">Jewelery</Link>
+										</li>
+									</ul>
+								)}
+							</div>
 						</li>
 						<li>
-							<BurgerMenu />
+							<Link className={router.pathname === "/about" ? styles.active : ""} href="/about">
+								About
+							</Link>
 						</li>
-					</div>
+						<li>
+							<Link className={router.pathname === "/contact" ? styles.active : ""} href="/contact">
+								Contact
+							</Link>
+						</li>
+						{status === "authenticated" && data !== null && (
+							<li>
+								<Link className={router.pathname === "/dashboard" ? styles.active : ""} href="/dashboard">
+									Dashboard
+								</Link>
+							</li>
+						)}
+					</ul>
+				</nav>
+				<div className={styles.list_icon}>
+					{status === "unauthenticated" && data === null && (
+						<li>
+							<Link href="/login">
+								Login
+								<PersonIcon />
+							</Link>
+						</li>
+					)}
+					{status === "authenticated" && data !== null && (
+						<li>
+							<Link href="/dashboard">
+								{data.user.username}
+								<PersonIcon />
+							</Link>
+						</li>
+					)}
+					<li onClick={() => setIsSearchOpen(true)}>
+						<SearchIcon />
+					</li>
+					<li className={styles.shoppingIcon_li} onClick={() => dispatch(setIsCartOpen({}))}>
+						{cartCount > 0 && <span>{cartCount}</span>}
+						<ShoppingBasketIcon />
+					</li>
+					<li>
+						<BurgerMenu />
+					</li>
 				</div>
 			</div>
 			{isCartOpen && <ShoppingCart />}
