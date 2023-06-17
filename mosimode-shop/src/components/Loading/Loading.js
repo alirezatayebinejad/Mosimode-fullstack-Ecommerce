f0b2573064acbef7766e12a84fc667bffe0cd5a2
vibracode <codevibra@gmail.com>
2023-06-17 18:55:35 +0430

    loading spinner bug fixed

--- mosimode-shop/src/components/Loading/Loading.js
+++ mosimode-shop/src/components/Loading/Loading.js
@@ -6,13 +6,14 @@ import Image from 'next/image';
 const Loading = ({ spinnerType }) => {
     return (
         <>
-            {spinnerType === "progress" ? <LinearProgress color="inherit" /> :
-                <div className={styles.loading_container}>
+            <div className={styles.loading_container}>
+                {spinnerType === "progress" ? <LinearProgress color="inherit" /> :
                     <div className={styles.loading_part}>
                         {spinnerType === "spinner1" && <Image src={"/assets/loaders/spinner1.svg"} width={100} height={100} alt={"spinner icon animated"} />}
                         {spinnerType === "spinner2" && <Image src={"/assets/loaders/spinner2.svg"} width={100} height={100} alt={"spinner icon animated"} />}
                     </div>
-                </div>}
+                }
+            </div>
         </>
     )
 }
