e9161b7e10d9966751bc189a5992e3b4187275be
vibracode <codevibra@gmail.com>
2023-06-17 17:09:34 +0430

    spinner on router change added

---
+++ mosimode-shop/src/components/Loading/Loading.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from "./Loading.module.css"
+import LinearProgress from '@mui/material/LinearProgress';
+import Image from 'next/image';
+
+const Loading = ({ spinnerType }) => {
+    return (
+        <>
+            {spinnerType === "progress" ? <LinearProgress color="inherit" /> :
+                <div className={styles.loading_container}>
+                    <div className={styles.loading_part}>
+                        {spinnerType === "spinner1" && <Image src={"/assets/loaders/spinner1.svg"} width={100} height={100} alt={"spinner icon animated"} />}
+                        {spinnerType === "spinner2" && <Image src={"/assets/loaders/spinner2.svg"} width={100} height={100} alt={"spinner icon animated"} />}
+                    </div>
+                </div>}
+        </>
+    )
+}
+
+export default Loading
--- mosimode-shop/src/pages/_app.js
+++ mosimode-shop/src/pages/_app.js
@@ -3,11 +3,33 @@ import { store } from "../store/store";
 import { Provider } from "react-redux";
 import MessagePopup from '@/components/Popups/MessagePopup';
 import { SessionProvider } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Loading from '../components/Loading/Loading';
 
 export default function App({ Component, pageProps }) {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleStart = () => setLoading(true);
+    const handleComplete = () => setLoading(false);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, []);
+
   return (
     <SessionProvider>
       <Provider store={store}>
+        {loading ? <Loading spinnerType={"progress"} /> : null}
         <Component {...pageProps} />
         <MessagePopup />
       </Provider>
