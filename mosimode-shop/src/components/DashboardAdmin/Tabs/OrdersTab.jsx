ee1e1b609ccc7bd5b6c58fe8e8d4146d1baede7e
vibracode <codevibra@gmail.com>
2023-06-23 04:27:39 +0430

    admin: ordertab and users tab

--- mosimode-shop/src/components/DashboardAdmin/Tabs/OrdersTab.jsx
+++ mosimode-shop/src/components/DashboardAdmin/Tabs/OrdersTab.jsx
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./OrdersTab.module.css";
+import axios from "axios";
 
 const OrdersTab = () => {
-	return <div>OrdersTab</div>;
+	const [orders, setOrders] = useState([]);
+
+	useEffect(() => {
+		fetchOrders();
+	}, []);
+
+	const fetchOrders = async () => {
+		try {
+			const response = await axios.get("/api/getOrders");
+			setOrders(response.data);
+		} catch (error) {
+			console.error("Error fetching orders:", error);
+		}
+	};
+
+	return (
+		<div className={styles.ordersTab}>
+			{orders.length > 0 ? (
+				<ul className={styles.orderList}>
+					{orders.map((order) => (
+						<li key={order.id} className={styles.orders}>
+							<div className={styles.orderDetails}>
+								<div className={styles.orderHeader}>
+									<div className={styles.orderId}>Order ID: {order.id}</div>
+									<div className={styles.paymentStatus}>status: {order.paymentStatus}</div>
+									<div className={styles.orderAmount}>Total Amount: {order.totalAmount}</div>
+								</div>
+								<div className={styles.orderItems}>
+									<h3>Order Items:</h3>
+									<ul className={styles.orderItemList}>
+										{order.items.map((item) => (
+											<li key={item.id} className={styles.orderItem}>
+												<div className={styles.itemId}>id: {item.product.id}</div>
+												<div className={styles.itemName}>{item.product.title}</div>
+
+												<div className={styles.itemQuantity}>Quantity: {item.quantity}</div>
+												<div className={styles.itemPrice}>Price: {item.price}</div>
+											</li>
+										))}
+									</ul>
+								</div>
+							</div>
+							<div className={styles.orderAddress}>
+								<h3>Delivery Address:</h3>
+								<div className={styles.addressLine}>name: {order.name}</div>
+								<div className={styles.addressLine}>phone number: {order.phonenumber}</div>
+								<div className={styles.addressLine}>address: {order.address}</div>
+								<div className={styles.addressLine}>city: {order.city}</div>
+								<div className={styles.addressLine}>state: {order.state}</div>
+								<div className={styles.addressLine}>postal code: {order.postalcode}</div>
+								<div className={styles.addressLine}>delivery: {order.deliveryMethod}</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			) : (
+				<div className={styles.noOrders}>No orders found.</div>
+			)}
+		</div>
+	);
 };
 
 export default OrdersTab;
--- mosimode-shop/src/components/DashboardAdmin/Tabs/UsersTab.jsx
+++ mosimode-shop/src/components/DashboardAdmin/Tabs/UsersTab.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./UsersTab.module.css";
+import { useDispatch } from "react-redux";
+import { openPopup } from "@/store/messagePopupSlice";
+import axios from "axios";
+
 const UsersTab = () => {
-	return <div>UsersTab</div>;
+	const dispatch = useDispatch();
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	const fetchUsers = async () => {
+		try {
+			const response = await axios.get("/api/admin/getUsers");
+			setUsers(response.data);
+		} catch (error) {
+			console.error("Error fetching users:", error);
+		}
+	};
+
+	const removeUser = async (userId) => {
+		try {
+			await axios.delete(`/api/admin/deleteUser?id=${userId}`);
+			const newUsersList = users.filter((item) => {
+				return item.id !== userId;
+			});
+			setUsers(newUsersList);
+			dispatch(openPopup({ message: "the user is removed", mood: true }));
+		} catch (error) {
+			dispatch(openPopup({ message: "there was a problem removing user", mood: true }));
+			console.error("Error removing user:", error);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.countUsers}>
+				<p>total products: {users.length}</p>
+			</div>
+			{users.map((user) => (
+				<div key={user.id} className={styles.userCard}>
+					<p>{user.username}</p>
+					<button onClick={() => removeUser(user.id)}>Remove</button>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default UsersTab;
